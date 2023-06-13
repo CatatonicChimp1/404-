0c3e216cdff4b78aa75fee960cfad28a069ffc5d
korneevnr <114693945+korneevnr@users.noreply.github.com>
2023-06-13 18:28:49 +0300

    Blocking user

--- serverIntelliTestinh/src/main/java/com/example/serverIntelliTestinh/servive/impl/UserServiceImpl.java
+++ serverIntelliTestinh/src/main/java/com/example/serverIntelliTestinh/servive/impl/UserServiceImpl.java
@@ -4,11 +4,11 @@ import com.example.serverIntelliTestinh.model.User;
 import com.example.serverIntelliTestinh.repo.UserRepo;
 import com.example.serverIntelliTestinh.repo.repoImpl.UserRepoImpl;
 import com.example.serverIntelliTestinh.servive.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -44,6 +44,20 @@ public class UserServiceImpl implements UserService {
     @Override
     public void update(User user, String login) throws IOException {
         UserRepo repo = new UserRepoImpl();
+        repo.update(user, login);
+    }
+
+    @Override
+    public User[] getAll() throws FileNotFoundException, JsonProcessingException {
+        UserRepo repo = new UserRepoImpl();
+        return repo.getAll();
+    }
+
+    @Override
+    public void isBlocked(String login, boolean isBlocked) throws IOException {
+        UserRepo repo = new UserRepoImpl();
+        User user = repo.searchByLogin(login);
+        user.setBlocked(isBlocked);
         repo.update(user, login);
     }
 }
