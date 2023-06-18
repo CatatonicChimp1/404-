a9b1f34d45489ec9a8692cea1dbfb0a4a31d6eaf
korneevnr <114693945+korneevnr@users.noreply.github.com>
2023-06-18 15:33:27 +0300

    bug fix

--- intelligentTesting/src/main/java/org/example/model/Test/Test.java
+++ intelligentTesting/src/main/java/org/example/model/Test/Test.java
@@ -1,15 +1,45 @@
 package org.example.model.Test;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 import java.util.HashMap;
+import java.util.List;
 
 public class Test {
-    private HashMap<String, Question> question;
+    private int id;
+    private String question;
+    private List<String> answer;
+    private int idTrueAnswer;
+    public int getId() {
+        return id;
+    }
 
-    public HashMap<String, Question> getQuestion() {
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
         return question;
     }
 
-    public void setQuestion(HashMap<String, Question> question) {
+    public void setQuestion(String question) {
         this.question = question;
+    }
+
+    public List<String> getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(List<String> answer) {
+        this.answer = answer;
+    }
+
+    public int getIdTrueAnswer() {
+        return idTrueAnswer;
+    }
+
+    public void setIdTrueAnswer(int idTrueAnswer) {
+        this.idTrueAnswer = idTrueAnswer;
     }
 }
