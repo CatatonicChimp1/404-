0ddae6813c8d29c57ceffc289dc38f6ac706e2f7
korneevnr <114693945+korneevnr@users.noreply.github.com>
2023-06-19 11:18:44 +0300

    Update TestController.java

--- serverIntelliTestinh/src/main/java/com/example/serverIntelliTestinh/controller/TestController.java
+++ serverIntelliTestinh/src/main/java/com/example/serverIntelliTestinh/controller/TestController.java
@@ -35,7 +35,12 @@ public class TestController {
     }
 
     @RequestMapping(path = "/all",method = RequestMethod.GET)
-    public ResponseEntity<Test> getAll() throws FileNotFoundException, JsonProcessingException {
+    public ResponseEntity<Test[]> getAll() throws FileNotFoundException, JsonProcessingException {
         return ResponseEntity.ok(service.getAll());
+    }
+
+    @RequestMapping(path = "/import", method = RequestMethod.POST)
+    public void importTest(@RequestBody Test[] tests){
+        service.importTest(tests);
     }
 }
